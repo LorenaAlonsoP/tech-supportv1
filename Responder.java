595e868be4afe5ee2739981a0d305edc6888c26b
Lorena Alonso <lorenaalonsop26@gmail.com>
2018-02-15 22:34:38 +0100

    Añadidos los cambios del ejercicio 0092

--- Responder.java
+++ Responder.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -12,6 +12,7 @@ public class Responder
 {
     Random aleatorio;
     ArrayList<String> respuestas;
+    HashMap<String, String> respuestasInput;
     /**
      * Construct a Responder - nothing to do
      */
@@ -19,20 +20,30 @@ public class Responder
     {
         aleatorio = new Random();
         respuestas = new ArrayList<>();
+        respuestasInput = new HashMap<>();
         respuestas.add("Are you sure?");
         respuestas.add("We are working on your problem.");
         respuestas.add("Wait a minute!");
         respuestas.add("Can you give me more information?");
         respuestas.add("Let's do it!");
+        respuestasInput.put("Windows", "We work with Windows, not with Linux.");
+        respuestasInput.put("Linux", "We don't use this software, sorry.");
+        respuestasInput.put("IOS", "We don't use this software, sorry.");
+        respuestasInput.put("Sell", "We can see the sellers.");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String input1)
     {
         int numAleatorio = aleatorio.nextInt(respuestas.size());
+        String respuesta = null;
+        respuesta = respuestasInput.get(input1);
+        if(respuesta == null) {
+            respuesta = respuestas.get(aleatorio.nextInt(respuestas.size()));
+        }
         return respuestas.get(numAleatorio);
     }
 }
